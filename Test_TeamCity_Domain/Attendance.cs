e6dc4eb2e14d8839f588316d7bc0bb3229264708
Srimathi Mani <manis@parliament.uk>
2025-05-11 21:21:44 +0100

    Initial commit of Test_TeamCity

---
+++ Test_TeamCity_Domain/Attendance.cs
@@ -0,0 +1,11 @@
+﻿namespace Test_TeamCity_Domain
+{
+    public class Attendance
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public bool IsPresent { get; set; }
+    }
+
+}
---
+++ Test_TeamCity_Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Test_TeamCity_Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
